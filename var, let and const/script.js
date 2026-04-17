a0a4f69bb2def77e0103f21f2047d13077fb231e
code-azeemahmad <azeemahmadd9@gmail.com>
2026-04-17 10:47:21 +0500

    story behind var, let and const

---
+++ var, let and const/script.js
@@ -0,0 +1,15 @@
+const accountId = 123;
+let accountEmail = "azeem@google.com";
+
+var accountPassword = "ha[[y";      // don't use var because of issue in block scoped and functional scope
+
+accountCity = "shibuya";
+
+let accountState;
+
+// accountId = 456;
+accountEmail = "hello@hello.com";
+accountCity = "tokyo";
+
+console.log(accountCity);
+console.log(accountState);
