c50c1aba15dcbbd09e5b1dbbe37155604d8101eb
code-azeemahmad <azeemahmadd9@gmail.com>
2026-04-17 11:00:59 +0500

    datatypes and ecma script standards

---
+++ datatypes/script.js
@@ -0,0 +1,20 @@
+"use strict";   // treat all js code as newer version
+
+// alert(3+3)   // we are using node js, not browser
+
+let name = "azeem";
+let age = 20;
+let isLoggedIn = true;
+let state = null;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean
+// null
+// undefined
+// symbol => uniqueness
+
+// object
+
+console.log(typeof undefined, null);
