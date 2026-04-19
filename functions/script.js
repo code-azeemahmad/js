3869e8d3766c808eaf3dc4ee0c0c5af3b14cf76d
code-azeemahmad <azeemahmadd9@gmail.com>
2026-04-19 06:51:15 +0500

    function reference and execution

---
+++ functions/script.js
@@ -0,0 +1,10 @@
+function sayMyName() {
+  console.log("A");
+  console.log("Z");
+  console.log("E");
+  console.log("E");
+  console.log("M");
+}
+
+sayMyName; // reference
+sayMyName(); // execution
