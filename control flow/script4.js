72e272bf3ca88355d2d937150aafd78a3a2955ae
code-azeemahmad <azeemahmadd9@gmail.com>
2026-04-19 16:39:26 +0500

    Ternary Operator

---
+++ control flow/script4.js
@@ -0,0 +1,10 @@
+// Ternary operator
+
+// condition ? valueIfTrue : valueIfFalse
+
+const iceTeaPrice = 100;
+iceTeaPrice >= 80 ? console.log(`greater than 80`) : console.log(`less than 80`);
+
+const age = 18;
+const status = age >= 18 ? "Adult" : "Minor";
+console.log(status);
