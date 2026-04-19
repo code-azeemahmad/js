101310acfaa62b1fccccafaf0a008f60f2ecba17
code-azeemahmad <azeemahmadd9@gmail.com>
2026-04-19 06:59:10 +0500

    Function parameters, arguments, return result

--- functions/script.js
+++ functions/script.js
@@ -7,4 +7,22 @@ function sayMyName() {
 }
 
 sayMyName; // reference
-sayMyName(); // execution
+// sayMyName(); // execution
+
+// function addTwoNumbers(num1, num2) {    // parameters
+//     console.log(num2 + num1);
+// }
+
+// addTwoNumbers();    // NaN
+addTwoNumbers(3, 4);    // arguments
+// addTwoNumbers(3, '4');
+// addTwoNumbers(3, 'a');
+// addTwoNumbers(3, null);
+
+function addTwoNumbers(num1, num2) {
+    return num1 + num2;
+    // console.log(num1 + num2);   // unreachable code
+}
+
+const num3 = addTwoNumbers(3, 4);
+console.log(num3);
