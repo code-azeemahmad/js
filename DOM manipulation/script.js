de14d1dc9bf3b6fb44b49a8c7318a0f759f16648
code-azeemahmad <azeemahmadd9@gmail.com>
2026-04-22 19:46:21 +0500

    intro to DOM manipulation and document object model in js

---
+++ DOM manipulation/script.js
@@ -0,0 +1,8 @@
+console.log(window);
+console.log(window.document);
+console.log(document);
+console.dir(document);
+console.log(document.baseURI);
+console.log(document.links);
+
+// https://excalidraw.com/#json=BkiviOCeWzc8Atu4kFmCh,H_VHYU6n90DCisFztXiGiQ
