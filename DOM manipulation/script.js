fe35832aeab82d134caa12ae0f09753ee54a3b71
code-azeemahmad <azeemahmadd9@gmail.com>
2026-04-23 19:28:09 +0500

    getAttribute and setAttribute and style in DOM manipulation

--- DOM manipulation/script.js
+++ DOM manipulation/script.js
@@ -6,3 +6,17 @@ console.log(document.baseURI);
 console.log(document.links);
 
 // https://excalidraw.com/#json=BkiviOCeWzc8Atu4kFmCh,H_VHYU6n90DCisFztXiGiQ
+
+let title = document.getElementById(`title`);
+title.id;
+title.class;  // undefined
+title.className;
+title.getAttribute('id');
+title.getAttribute('class');
+title.setAttribute('class');
+title.setAttribute('class', 'bg-green');  // overwrite previous attribute
+title.setAttribute('class', 'heading bg-green');  // set multiple attributes
+
+title.style.backgroundColor = 'green';
+title.style.padding = '15px';
+title.style.borderRadius = '10px';
