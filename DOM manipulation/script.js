c0b50ea9369e69ac78a671170706516a02a0b012
code-azeemahmad <azeemahmadd9@gmail.com>
2026-04-23 20:03:11 +0500

    querySelector and querySelectorAll and node list manipulation

--- DOM manipulation/script.js
+++ DOM manipulation/script.js
@@ -23,4 +23,23 @@ title.style.borderRadius = '10px';
 
 title.innerText;  //  returns only the unhidden text
 title.textContent;  // returns the entire text even if style="display: none;
-title.innerHTML;  // returns the text + htmt
+title.innerHTML;  // returns the text + html
+
+document.querySelector(`#title`);
+document.querySelector(`.heading`);
+document.querySelector(`p`);  // returns the first element that matches the specified CSS selector
+document.querySelector(`input[type = 'password']`);
+
+document.querySelector('ul li:first-child').style.backgroundColor = 'red';
+document.querySelector('ul li:first-child').innerText = 'hello ul 1';
+
+document.querySelectorAll('ul li'); // returns a node list(different from array, limited methods(no map, filter, reduce))
+let arr = document.querySelectorAll('ul li');
+arr[0].style.backgroundColor = 'green';
+arr.forEach((c) => c.style.backgroundColor = 'red');
+
+let h = document.querySelectorAll('h1');  // returns node list even for a single element
+// h.style.backgroundColor = 'green';  // X
+h[0].style.backgroundColor = 'green';
+
+
